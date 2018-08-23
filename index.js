dbc9fd6d4305df84dbd9d24d5dcfbe79a1bd7c06
Kiki Dolan <cdolan323@gmail.com>
2018-08-23 23:25:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
   console.log(movies)
+  
+  const titanicButton = document.getElementById('titanic')
 });
