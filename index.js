eb3f18363402fe40af37e0aee3511c0277971141
Kiki Dolan <cdolan323@gmail.com>
2018-08-23 23:42:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ let title = document.getElementById('title')
 let director = document.getElementById('director')
 let genre = document.getElementById('genre')
 let filmRating = document.getElementById('filmRating')
+let poster = document.getElementById('poster')
 
   title.innerHTML = movies['Titanic'].title
   director.innerHTML = movies['Titanic'].director
   genre.innerHTML = movies['Titanic'].genre
   filmRating.innerHTML = movies['Titanic'].filmRating
+  poster.setAttribute('src', movies['Titanic'].poster)
   } 
 
   
