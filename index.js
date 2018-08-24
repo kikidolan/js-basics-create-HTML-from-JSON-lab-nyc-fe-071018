68c21611c3c4f1ce41a535b11029219189d1667f
Kiki Dolan <cdolan323@gmail.com>
2018-08-24 00:41:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-  // invoking a function here will make sure it happens on page load
-  // your code here
-  updateHTMLContent('Titanic')
-  const titanicButton = document.getElementById('Titanic')
-  const terminatorButton = document.getElementById('Terminator 2')
-  titanicButton.addEventListener('click', (event) => {
-    //do something
-    updateHTMLContent('Titanic')
-     
-  })
-  terminatorButton.addEventListener('click',(event) => {
-    //do something
-    updateHTMLContent('Terminator 2')
-  })
-});
+updateHTMLContent('Titanic')
 
 function updateHTMLContent(movieName) {
+  let currentMovie = movies[movieName]
   let title = document.getElementById('title')
   let director = document.getElementById('director')
   let genre = document.getElementById('genre')
@@ -23,10 +9,9 @@ function updateHTMLContent(movieName) {
   let poster = document.getElementById('poster')
   let description = document.getElementById('description')
   let audienceScore = document.getElementById('audienceScore')
-  let currentMovie = movies[movieName]
-  let cast = document.getElementById('cast')
+  let cast = document.getElementById('cast')  
   let reviews = document.getElementById("reviews")
-
+  
   title.innerHTML = currentMovie.title
   director.innerHTML = currentMovie.director
   genre.innerHTML = currentMovie.genre
@@ -37,33 +22,35 @@ function updateHTMLContent(movieName) {
   let castInnerHTML = ""
   let reviewInnerHTML = ""
   
-  for (let i = 0; i < currentMovie.cast.length; i++) {
-    let role = currentMovie.cast[i].role
-    let actor = currentMovie.cast[i].actor
-    castInnerHTML += ("<li>" + role + ":" + actor +"</li>")
+  for (let castMember of currentMovie.cast) {
+    let role = castMember.role
+    let actor = castMember.actor
+    castInnerHTML += ("<p>" + role + ": " + actor + "</p>")  
   }
-  castInnerHTML += "</ul>"
+  castInnerHTML +=""
   cast.innerHTML = castInnerHTML
   
   for (let review of currentMovie.reviews) {
-    let userName = review.username
+    let username = review.username
     let content = review.content
-    reviewInnerHTML += ("<p>" + userName + ":" + content + "<p>")
+    reviewInnerHTML += ("<p>" + username + ": " + content + "</p>")
   }
+  reviewInnerHTML += ""
+  reviews.innerHTML = reviewInnerHTML
 
-reviewInnerHTML += ""
-reviews.innerHTML = reviewInnerHTML
-}
-
-  updateHTMLContent('Titanic')
   const titanicButton = document.getElementById('Titanic')
   const terminatorButton = document.getElementById('Terminator 2')
   titanicButton.addEventListener('click', (event) => {
-    //do something
+    //do something titanic
     updateHTMLContent('Titanic')
+  })
   terminatorButton.addEventListener('click', (event) => {
+    //do something terminator
     updateHTMLContent('Terminator 2')
   })
-     
-  })
+}
+document.addEventListener("DOMContentLoaded", (e) => {
+  // invoking a function here will make sure it happens on page load
+  // your code here
   
+})
